8c4a9327173dc7eeb62c2f314b8506afbe6763dd
Chris Ziegler <chrisrziegler@gmail.com>
2021-10-08 01:11:33 -0700

    Prepare for Heroku Deployment commit

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 
 // const app = http.createServer((req, res) => {
@@ -98,7 +100,7 @@ const unkownEndpoint = (request, response) => {
 
 app.use(unkownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`)
 })
