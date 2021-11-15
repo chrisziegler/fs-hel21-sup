47ea4d4d77c90068c8bea15b0c3bd22e4e6d6171
Chris Ziegler <chrisrziegler@gmail.com>
2021-11-14 22:27:17 -0800

    Extract components and use react-redux

--- part6/redux-notes/src/index.js
+++ part6/redux-notes/src/index.js
@@ -1,47 +1,15 @@
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './App'
 import noteReducer from './reducers/noteReducer'
 import './index.css'
 
 const store = createStore(noteReducer)
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1,
-  },
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2,
-  },
-})
-
-const App = () => {
-  return (
-    <div>
-      <h1>Redux Notes</h1>
-      <ul>
-        {store.getState().map(note => (
-          <li key={note.id}>
-            {note.content}{' '}
-            <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
-
-const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById('root'))
-}
-
-renderApp()
-store.subscribe(renderApp)
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+)
