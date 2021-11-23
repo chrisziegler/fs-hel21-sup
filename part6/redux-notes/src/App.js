2bbec3483bf02fe98f5125b1bdaf9f518310d654
Chris Ziegler <chrisrziegler@gmail.com>
2021-11-22 23:06:24 -0800

    JSON server added to persist notes and to make asynchronous requests from a server before dispatching to App store in redux

--- part6/redux-notes/src/App.js
+++ part6/redux-notes/src/App.js
@@ -1,8 +1,16 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
+import * as noteService from './services/notes'
+import { initializeNotes } from './reducers/noteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    noteService.getAll().then(notes => dispatch(initializeNotes(notes)))
+  }, [dispatch])
   return (
     <div>
       <NewNote />
