104c6cc4b57bce71907d49c4fbda8ce1d98ae066
Chris Ziegler <chrisrziegler@gmail.com>
2021-12-20 20:41:00 -0800

    Change config file, and add .babelrc for actual Webpack 5+ set-up, resolve errors

--- part7/webpack-part7/webpack.config.js
+++ part7/webpack-part7/webpack.config.js
@@ -1,10 +1,24 @@
 const path = require('path')
 
-const config = {
-  entry: './src/index.js',
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js'],
+  },
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, './build'),
     filename: 'main.js',
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, './public'),
+  },
 }
-module.exports = config
