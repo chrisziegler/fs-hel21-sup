faea8c676add17d1340a9d21f6746161df45f4bf
Chris Ziegler <chrisrziegler@gmail.com>
2021-12-21 21:34:58 -0800

    Advanced Webpack configuration - environment set-up

--- part7/webpack-part7/webpack.config.js
+++ part7/webpack-part7/webpack.config.js
@@ -1,33 +1,49 @@
 const path = require('path')
+const webpack = require('webpack')
 
-module.exports = {
-  entry: path.resolve(__dirname, './src/index.js'),
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx'],
-  },
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'main.js',
-  },
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, './build'),
+const config = (env, argv) => {
+  console.log('argv', argv.mode, 'env:', env)
+
+  const backend_url =
+    argv.mode === 'production'
+      ? 'https://blooming-atoll-75500.herokuapp.com/api/notes'
+      : 'http://localhost:3001/notes'
+
+  return {
+    entry: path.resolve(__dirname, './src/index.js'),
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
     },
-    compress: true,
-    hot: true,
-    port: 3000,
-  },
+    resolve: {
+      extensions: ['*', '.js', '.jsx'],
+    },
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'main.js',
+    },
+    devServer: {
+      static: path.resolve(__dirname, 'build'),
+      compress: true,
+      hot: true,
+      port: 3000,
+    },
+    devtool: 'source-map',
+    plugins: [
+      new webpack.DefinePlugin({
+        BACKEND_URL: JSON.stringify(`${backend_url}`),
+      }),
+    ],
+  }
 }
+
+module.exports = config
